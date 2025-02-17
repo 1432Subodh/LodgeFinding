bd9d0680376c32d6ea3c9a92b14efbda73c6e4f3
Subodh Ravidas <subodh14329@gmail.com>
2025-02-17 16:39:58 +0530

    updating

--- src/app/api/user/auth/login/route.ts
+++ src/app/api/user/auth/login/route.ts
@@ -1,47 +1,87 @@
-import { NextRequest, NextResponse } from "next/server";
-import userSchema from "../../../../../../models/UserModel";
+// import { NextRequest, NextResponse } from "next/server";
+// import userSchema from "../../../../../../models/UserModel";
 import bcryptjs from 'bcryptjs'
 import jwt from 'jsonwebtoken'
 
+import { NextRequest, NextResponse } from "next/server";
+import userSchema from "../../../../../../models/UserModel";
+import { connect_db } from '../../../../../../utils/connect';
 
-export async function POST(request: NextRequest) {
+
+// export async function POST(request: NextRequest) {
+//     const reqBody = await request.json()
+
+//     const { password, email, remember } = reqBody
+
+//     const user = await userSchema.findOne({ email })
+
+//     if (!user) {
+//         return NextResponse.json({
+//             message: "user not found",
+//             success: false
+
+//         },{status:202})
+//     }
+//     const validation = await bcryptjs.compare(password,user.password)
+//     if(!validation){
+//         return NextResponse.json({
+//             message: "Incorrect Password",
+//             success: false
+//         })
+//     }
+
+
+//     const token = jwt.sign({
+//         email : user.email,
+//         userId : user._id,
+//     }, process.env.SCRECT_KEY!,{expiresIn : `${remember=='on' ? '30d': '1d'}`})
+
+//     const response = NextResponse.json({
+//         message: 'login successfull',
+//         success: true
+//     },{status:200})
+
+//     response.cookies.set('token', token,{
+//         httpOnly: true,
+//     })
+//     return response
+
+
+    
+// }
+
+connect_db()
+
+export async function POST(request:NextRequest) {
     const reqBody = await request.json()
+    const{email, password, remember} = reqBody;
 
-    const { password, email, remember } = reqBody
+    const isUser = await userSchema.findOne({email})
 
-    const user = await userSchema.findOne({ email })
-
-    if (!user) {
-        return NextResponse.json({
-            message: "user not found",
-            success: false
-
-        },{status:202})
+    if(!isUser){
+        return NextResponse.json({message: 'User not exits first login'})
     }
-    const validation = await bcryptjs.compare(password,user.password)
-    if(!validation){
-        return NextResponse.json({
-            message: "Incorrect Password",
-            success: false
-        })
+
+    const validatedPassword = await bcryptjs.compare(password, isUser.password)
+
+    if(!validatedPassword){
+        return NextResponse.json({message: 'Invalid Password'})
     }
 
 
     const token = jwt.sign({
-        email : user.email,
-        userId : user._id,
-    }, process.env.SCRECT_KEY!,{expiresIn : `${remember=='on' ? '30d': '1d'}`})
+        userId : isUser._id,
+        email: isUser.email,
+    }, process.env.SCRECT_KEY!)
+
 
     const response = NextResponse.json({
-        message: 'login successfull',
+        message: 'Login Successfull',
         success: true
-    },{status:200})
+    },{status: 200})
+    
+    response.cookies.set('token', token)
 
-    response.cookies.set('token', token,{
-        httpOnly: true,
-    })
     return response
 
-
-    
 }
--- src/app/api/user/profile/route.ts
+++ src/app/api/user/profile/route.ts
@@ -1,8 +1,9 @@
 import { NextRequest, NextResponse } from "next/server";
 import { extractCookies } from "../../../../../helper/extractCookies";
 import userSchema from "../../../../../models/UserModel";
+import { connect_db } from "../../../../../utils/connect";
 
-
+connect_db()
 export async function GET(request: NextRequest) {
     try {
         const userId = await extractCookies(request)
