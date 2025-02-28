0d80a59d1cc86f1813f69cd9b3a4a716b9b1ee13
Subodh Ravidas <subodh14329@gmail.com>
2025-02-28 14:39:18 +0530

    up

--- src/app/(Lodge-Section)/lodge/page.tsx
+++ src/app/(Lodge-Section)/lodge/page.tsx
@@ -3,7 +3,9 @@ import React, { Suspense } from 'react'
 
 function page() {
   return (
-    <Suspense fallback={<div>Loading...</div>}>
+    <Suspense fallback={<div className="flex items-center justify-center min-h-screen">
+      <div className="w-12 h-12 border-4 border-primary border-t-transparent rounded-full animate-spin"></div>
+    </div>}>
     <LodgeSection />
   </Suspense>
   )
