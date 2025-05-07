61c6574836f02d7d2545ebb3f5aa9599c6636b1c
Subodh Ravidas <subodh14329@gmail.com>
2025-05-07 21:49:55 +0530

    adding dynamic title

--- src/app/(Lodge-Section)/lodge/view/[id]/page.tsx
+++ src/app/(Lodge-Section)/lodge/view/[id]/page.tsx
@@ -1,11 +1,38 @@
 import React from 'react'
 import ViewWarpper from './viewWarpper'
 
+import { Metadata } from 'next';
 
-export const metadata = {
-  title: "About Us - My Website",
-  description: "This is the about page of our amazing website.",
+type Props = {
+  params: { id: string };
 };
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const { id } = params;
+  console.log(id)
+
+  // Use full URL if fetching server-side from an API
+  const res = await fetch(`${process.env.DOMAIN}api/lodge/get`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ id }),
+    // Ensure it doesn't cache (optional, for dynamic data)
+    cache: 'no-store'
+  });
+
+  const data = await res.json();
+
+  return {
+    title: data.lodge?.lodgeName || 'Lodge Hazar',
+    description: data.lodge?.description || 'Default Description',
+    openGraph: {
+      title: data.lodge?.lodgeName,
+      description: data.lodge?.description,
+      images: [data.lodge?.thumbnailImage || '/default-og.jpg'],
+    },
+  };
+}
+
 
 function page() {
   return (
