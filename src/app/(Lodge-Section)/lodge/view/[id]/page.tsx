68054180f77f8e2bc7c9652e7b9fb9b21cd39325
Subodh Ravidas <subodh14329@gmail.com>
2025-05-11 22:26:23 +0530

    up

--- src/app/(Lodge-Section)/lodge/view/[id]/page.tsx
+++ src/app/(Lodge-Section)/lodge/view/[id]/page.tsx
@@ -4,22 +4,34 @@ import ViewWarpper from './viewWarpper';
 type PageProps = any
 
 export async function generateMetadata({ params }: PageProps): Promise<Metadata> {
- const { id } = await params
-  const res = await fetch(`${process.env.DOMAIN}api/lodge/get`, {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ id }),
-    cache: 'no-store',
-  });
+  const { id } = await params;
+  console
 
-  const data = await res.json();
+  let data: any = {};
+  try {
+    const res = await fetch(`${process.env.DOMAIN}api/lodge/get`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ id }),
+      cache: 'no-store',
+    });
+
+    if (!res.ok) {
+      console.error("API fetch failed:", res.statusText);
+      throw new Error("API fetch error");
+    }
+
+    data = await res.json();
+  } catch (error) {
+    console.error("Metadata generation error:", error);
+  }
 
   return {
     title: data.lodge?.lodgeName || 'Lodge Hazar',
     description: data.lodge?.description || 'Default Description',
     openGraph: {
-      title: data.lodge?.lodgeName,
-      description: data.lodge?.description,
+      title: data.lodge?.lodgeName || 'Lodge Hazar',
+      description: data.lodge?.description || 'Default Description',
       images: [data.lodge?.thumbnailImage || '/default-og.jpg'],
     },
   };
